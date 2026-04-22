16857d7846e76de0719662bec1a4f8275c9489e0
DoubleGIF <62531504+DoubleGIF@users.noreply.github.com>
2026-04-22 09:45:31 +0800

    Update Start.java

--- AutoBackup/src/main/java/com/auto/Start.java
+++ AutoBackup/src/main/java/com/auto/Start.java
@@ -3,9 +3,7 @@ package com.auto;
 import com.auto.Ui.DeviceManagerUI;
 import javax.swing.*;
 
-
 public class Start {
-
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
